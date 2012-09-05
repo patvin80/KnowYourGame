5de53c38de7d21871fdbbf1d8d71429d9838d28d
vpatankar <vpatankar@solutionstreet.com>
2012-09-04 21:48:16 -0400

    Including Styles for the page and highlight and display code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.delete_all
+Player.delete_all
+
+Category.create(name:'Parents', is_active:true)
+Category.create(name:'Work Place', is_active:true)
+Category.create(name:'Grand Parents', is_active:true)
+Category.create(name:'Friends', is_active:true)
+Category.create(name:'Parents', is_active:true)
+Category.create(name:'Friends', is_active:true)
+Category.create(name:'Grand Parents', is_active:true)
+Category.create(name:'Work Place', is_active:true)
+
+Player.create(name:'Player 1', score: 0)
+Player.create(name:'Player 2', score: 0)
+Player.create(name:'Player 3', score: 0)
+Player.create(name:'Player 4', score: 0)
+Player.create(name:'Player 5', score: 0)
